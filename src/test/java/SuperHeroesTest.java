6c3b8532ed93423bf967fa50ee8cbe9a4297cfca
alexmwaura <alexmwaura43@gmail.com>
2020-11-04 15:06:17 +0300

    add test cases

---
+++ src/test/java/SuperHeroesTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class SuperHeroesTest {
+    @Test
+    public void getInstance_shouldBeInstance_true() throws Exception{
+        Map<String, Object> model = new HashMap<String, Object>();
+        ArrayList<String> features = new ArrayList<String>();
+        features.add("healer");
+        features.add("care giver");
+        model.put("id", 1);
+        model.put("group", "science");
+        model.put("features",features);
+        SuperHeroes superHeroes = new SuperHeroes(model);
+        assertEquals(true, superHeroes instanceof SuperHeroes);
+    }
+}
