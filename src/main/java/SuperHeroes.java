ad76a3a1412bb4817629c983e4407ef5cbc4c29c
alexmwaura <alexmwaura43@gmail.com>
2020-11-04 16:15:47 +0300

    add getter method

--- src/main/java/SuperHeroes.java
+++ src/main/java/SuperHeroes.java
@@ -3,9 +3,16 @@ import java.util.Map;
 
 public class SuperHeroes {
     private Map<String,Object> model;
-    private static ArrayList instances = new ArrayList();
+    private static ArrayList<SuperHeroes> instances = new ArrayList<SuperHeroes>();
     public SuperHeroes(Map<String,Object> heroObject){
        this.model = heroObject;
         instances.add(this);
+        model.put("id",instances.size());
+    }
+    public Object getModel(){
+        return model;
+    }
+    public static ArrayList<SuperHeroes> getAll(){
+        return instances;
     }
 }
