aff2c64e4fb0f381b7103e7c2a8a761b592f4bd2
alexmwaura <alexmwaura43@gmail.com>
2020-11-05 17:11:07 +0300

    add new routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,17 @@ public class App {
             Map<String, Object> model = new HashMap<String,Object >();
             return new ModelAndView(model,"heroes.hbs");
         }, new HandlebarsTemplateEngine());
+        get("/new-hero",(req,res)-> {
+            Map<String, Object> model = new HashMap<String,Object >();
+            return new ModelAndView(model,"new-hero.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/add_squad",(req,res)-> {
+            String squad = req.queryParams("squadName");
+            Map<String,Object> model = new HashMap<>();
+            model.put("squad", squad);
+            req.session().attribute("superhero",model);
+            return new ModelAndView(model, "new-hero.hbs");
+        },new HandlebarsTemplateEngine());
     }
 
 }
